e357294ee08dfcff6dd50fe2e161eaa0be04d244
nafidurmus <nafidurmus07@gmail.com>
2018-07-06 16:56:03 +0300

    Delete Marked Items Button

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+	def trash_em
+		@trash_em = List.where(:completed => true).destroy_all
+		redirect_to lists_url, notice: 'Bitmiş olan Yapılacaklar silindi.'
+	end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :lists
   root "lists#index"
+  get 'home/trash_em'
 end
