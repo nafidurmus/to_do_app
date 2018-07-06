ed3f3da06f12c05699f79b8a6c645538e927dd9c
nafidurmus <nafidurmus07@gmail.com>
2018-07-06 23:54:50 +0300

    Update Buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
 
-	def trash_em
-		@trash_em = List.where(:completed => true).destroy_all
+	def trash
+		@trash = List.where(:completed => true).destroy_all
 		redirect_to lists_url, notice: 'Bitmiş olan Yapılacaklar silindi.'
 	end
 
-	def trash_em_all
-		@trash_em_all = List.all.destroy_all
+	def trash_all
+		@trash_all = List.all.destroy_all
 		redirect_to lists_url, notice: 'Bütün liste silindi.'
 	end
 
